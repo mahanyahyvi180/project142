3cd07f0adbfa8e221fb82327d4ac81eb2eee0f20
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-26 20:33:04 +0330

    Add files via upload

---
+++ WindowsFormsApp38/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp38
+{
+  public class cMath
+    {
+        public int x;
+        public int y;
+
+        public void swap()
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
